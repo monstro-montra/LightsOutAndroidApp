f23e3efc7c5922bef80bc2b3f5bc68a641b8200a
monstro-montra <70246029+monstro-montra@users.noreply.github.com>
2023-02-10 03:52:46 -0500

    game complete

--- app/src/main/java/com/zybooks/lightsoutnav/MainActivity.java
+++ app/src/main/java/com/zybooks/lightsoutnav/MainActivity.java
@@ -1,8 +1,13 @@
 package com.zybooks.lightsoutnav;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 //controller class along with ColorFragment, HelpFragment, and GameFragment
 
 public class MainActivity extends AppCompatActivity {
@@ -11,5 +16,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.nav_host_fragment);
+
+        if(navHostFragment != null ){
+            NavController navController = navHostFragment.getNavController();
+
+            AppBarConfiguration appBarConfig = new AppBarConfiguration.Builder(
+                    R.id.navigation_game, R.id.navigation_color, R.id.navigation_help)
+                    .build();
+
+            NavigationUI.setupActionBarWithNavController(this, navController, appBarConfig);
+            NavigationUI.setupWithNavController(navView, navController);
+
+
+        }
     }
 }
